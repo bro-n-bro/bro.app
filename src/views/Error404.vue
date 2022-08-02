3180f65a5592af33f668ab82320e59c070d09d1a
Vitaliy Sulimenko <prevetal@gmail.com>
2022-08-02 13:01:22 +0300

    +

--- src/views/Error404.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <section class="error_info">
-        <div class="cont">
-            <img src="../assets/images/error_404.svg" alt="" class="img">
-
-            <div class="title">{{ $t('message.error_title_404') }}</div>
-        </div>
-    </section>
-</template>
