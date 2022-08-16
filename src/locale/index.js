94395c31b318a496325229c76e999ae63fbcdfa8
Vitaliy Sulimenko <prevetal@gmail.com>
2022-08-16 17:45:57 +0300

    Fix

--- src/locale/index.js
+++ src/locale/index.js
@@ -1,5 +1,5 @@
 import { createI18n } from 'vue-i18n'
-import VueI18nMessages from '@/locale/messages'
+import VueI18nMessages from '../locale/messages'
 
 
 const i18n = createI18n({
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,22 @@
-// import { fileURLToPath, URL } from "node:url";
-import path from 'path'
+import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-const pathSrc = path.resolve(__dirname, "./src")
-
+// https://vitejs.dev/config/
 export default defineConfig({
     define: {
         'process.env': {},
-        // '__VUE_I18N_FULL_INSTALL__': JSON.stringify(true),
-        // '__VUE_I18N_LEGACY_API__': JSON.stringify(true),
-        // '__INTLIFY_PROD_DEVTOOLS__': JSON.stringify(true)
+        '__VUE_I18N_FULL_INSTALL__': JSON.stringify(true),
+        '__VUE_I18N_LEGACY_API__': JSON.stringify(true),
+        '__INTLIFY_PROD_DEVTOOLS__': JSON.stringify(true)
     },
 
     plugins: [vue()],
 
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './src')
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
 });
